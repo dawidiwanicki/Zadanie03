5f1fa48d22deafb3c70c41dcc93693b0e1a73530
Dawid <dawid_i@tlen.pl>
2018-05-26 19:56:02 +0200

    While problem

--- 3_GuessGame/main.py
+++ 3_GuessGame/main.py
@@ -1,1 +1,20 @@
 # Your code here...
+import random
+
+flaga=False
+liczbaDoZgadniecia=random.randint(1,100)
+liczbaProb=0
+
+while flaga ==False:
+    zgaduj= int(input("Zgadnij liczbę z zakresu 1-100: "))
+    liczbaProb+=1
+
+    if zgaduj==liczbaDoZgadniecia:
+        flaga ==True
+        print("Gratulacje, zgadłeś. Poszukiwana liczba to: ", liczbaDoZgadniecia, "Udało się po tylu próbach: ", liczbaProb)
+    else:
+        if zgaduj>liczbaDoZgadniecia:
+            print("Liczba za duża")
+        else:
+            print("Liczba za mała")
+
