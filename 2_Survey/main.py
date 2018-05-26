aa938e1bdcae2616575662c8284e3edb52cf7299
Dawid <dawid_i@tlen.pl>
2018-05-26 19:29:55 +0200

    pTesting survey

--- 2_Survey/main.py
+++ 2_Survey/main.py
@@ -1,1 +1,21 @@
 # Your code here...
+wiek= int(input ("Ile masz lat? "))
+ileWgogo=""
+ilePiwaWweekend=""
+ileWieczorynek=""
+ileMufasa=""
+
+if wiek>=18:
+    ileWgogo=int(input("Ile razy byłeś w klubie gogo? "))
+    ilePiwaWweekend=int(input("Ile piw wypijasz w weekend? "))
+    print("Masz lat: ", wiek)
+    print("Tyle razy byłeś w klubie gogo: ", ileWgogo)
+    print("Tyle piw wypijasz w weekend: ", ilePiwaWweekend)
+
+else:
+    print("Ha, młodziak")
+    ileWieczorynek=int(input("Ile wieczorynek widziałeś w tym tygodniu "))
+    ileMufasa=int(input("Ile godzin płakałeś na Królu Lwie? "))
+print("Masz lat: ", wiek)
+print("Tyle wieczorynek widziałeś w tym tygodniu: ", ileWieczorynek)
+print("Tyle godzin płakałeś na Królu Lwie: ", ileMufasa)
